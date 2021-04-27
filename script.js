e24c29950e4743c42d225d6e0aa8b6ea9a0a0be7
Vlad <vlad2097544@gmail.com>
2021-04-27 10:28:09 +0300

    ADD switch

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-};
+// const personalMovieDB = {
+//         count: numberOfFilms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+// };
 
-const q1 = prompt('Один из последних просмотренных фильмов?', ''),
-        q2 = prompt('На сколько оцените его?', ''),
-        q3 = prompt('Один из последних просмотренных фильмов?', ''),
-        q4 = prompt('На сколько оцените его?', '');
+// const q1 = prompt('Один из последних просмотренных фильмов?', ''),
+//         q2 = prompt('На сколько оцените его?', ''),
+//         q3 = prompt('Один из последних просмотренных фильмов?', ''),
+//         q4 = prompt('На сколько оцените его?', '');
       
-personalMovieDB.movies[q1] = q2;
-personalMovieDB.movies[q3] = q4;
+// personalMovieDB.movies[q1] = q2;
+// personalMovieDB.movies[q3] = q4;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+ 
+const num = 50;
+switch (num) {
+        case 49:
+                console.log('No');
+                break;
+        case 100:
+                console.log('No');
+                break;
+        case 50:
+                console.log('Yes');
+                break;
+        default:
+                console.log('TTT');
+                break;
+
+}
