9d3fb529c44e7f4c57469105ad79d15428fc5830
Vlad <vlad2097544@gmail.com>
2021-04-27 12:07:18 +0300

    Practise 2

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 "use strict";
 
-// let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// const personalMovieDB = {
-//         count: numberOfFilms,
-//         movies: {},
-//         actors: {},
-//         genres: [],
-//         privat: false
-// };
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
 
-// const q1 = prompt('Один из последних просмотренных фильмов?', ''),
-//         q2 = prompt('На сколько оцените его?', ''),
-//         q3 = prompt('Один из последних просмотренных фильмов?', ''),
-//         q4 = prompt('На сколько оцените его?', '');
-      
-// personalMovieDB.movies[q1] = q2;
-// personalMovieDB.movies[q3] = q4;
-
-// console.log(personalMovieDB);
- 
-const num = 50;
-switch (num) {
-        case 49:
-                console.log('No');
-                break;
-        case 100:
-                console.log('No');
-                break;
-        case 50:
-                console.log('Yes');
-                break;
-        default:
-                console.log('TTT.');
-                break;
-
+for(let i = 0;i<2;i++){
+        const q1 = prompt('Один из последних просмотренных фильмов?', ''),
+        q2 = prompt('На сколько оцените его?', '');   
+        
+        if(q1 != null && q2 != null && q1 != '' && q2 != '' && q1.length < 50 && q2.length < 50){
+                personalMovieDB.movies[q1] = q2;
+                console.log('done');
+        } else {
+                i--;
+        }    
 }
+if(personalMovieDB.count < 10 ) {
+        console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        console.log('Вы классический зритель');
+} else {
+        console.log('Вы киноман');
+}
+
+console.log(personalMovieDB);
+ 
